472fb650d99816094abc4ff6798c69ffb9f5a7f5
Arturo Bernal <arturober@gmail.com>
2023-09-15 09:25:09 +0200

    functions

--- Unit1 - JavaScript/Part1/3.functions.js
+++ Unit1 - JavaScript/Part1/3.functions.js
@@ -6,7 +6,7 @@ function changeNum(n) {
 
 let n = 1;
 changeNum(n);
-console.log(n); // 
+console.log(n); // 1
 
 function changeArray(array) {
     array[0] = 999;
@@ -15,3 +15,23 @@ function changeArray(array) {
 let a = [1,2,3,4,5];
 changeArray(a);
 console.log(a.toString()); // 999,2,3,4,5
+
+function suma(n1, n2) {
+    // if(! (n1 instanceof Number) || ! (n2 instanceof Number)) {
+    //     throw Error("Parameters must be numbers!");
+    // }
+    return n1 + n2;
+}
+
+console.log(suma(2,6)); // 8
+console.log(suma("2", "6")); // 26
+console.log(suma(true, false)); // 1
+console.log(suma()); // NaN (undefined + undefined)
+console.log(suma("Hello")); // Helloundefined
+console.log(suma(1,2,3,4,5,6)); // 3 (1+2)
+
+let resta = function(n1, n2) {
+    return n1 - n2;
+};
+
+console.log(resta(7, 2));
