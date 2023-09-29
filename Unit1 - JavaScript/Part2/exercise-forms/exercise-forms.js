0916d3d022da4a482b2757f801b94340f272e5b7
Arturo Bernal <arturober@gmail.com>
2023-09-29 09:46:09 +0200

    forms

--- Unit1 - JavaScript/Part2/exercise-forms/exercise-forms.js
+++ Unit1 - JavaScript/Part2/exercise-forms/exercise-forms.js
@@ -4,13 +4,43 @@ let form = document.getElementById("form1");
 let imgPreview = document.getElementById("imgPreview");
 let tBody = document.querySelector("tbody");
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
+form.addEventListener("submit", e => {
+    e.preventDefault();
 
-  console.log(
-    Array.from(form.hobbies)
-      .filter((input) => input.checked)
-      .map((input) => input.value)
-      .join(", ")
-  );
+    let tdImg = document.createElement("td");
+    let img = document.createElement("img");
+    img.src = imgPreview.src;
+    tdImg.append(img);
+
+    let tdName = document.createElement("td");
+    tdName.append(form.name.value);
+
+    let tdHobbies = document.createElement("td");
+    tdHobbies.append(Array.from(form.hobbies).filter(i => i.checked).map(i => i.value).toString());
+
+    let tdFood = document.createElement("td");
+    tdFood.append(form.food.value);
+
+    let tdLang = document.createElement("td");
+    tdLang.append(form.language.value);
+    
+    let tr = document.createElement("tr");
+    tr.append(tdImg, tdName, tdHobbies, tdFood, tdLang);
+    tBody.append(tr);
+
+    form.reset();
+    imgPreview.src = "";
+});
+
+form.photo.addEventListener("change", e => {
+    let file = form.photo.files[0];
+    
+    if(file && file.type.startsWith("image")) {
+        let reader = new FileReader();
+        reader.readAsDataURL(file);
+
+        reader.addEventListener("load", e => {
+            imgPreview.src = reader.result;
+        });
+    }
 });
