494b6c5795a0df5329b0aa8c3f167d7f6b64b824
Arturo Bernal <arturober@gmail.com>
2023-10-10 09:46:46 +0200

    fetch

--- Unit1 - JavaScript/Part3/11.fetch.js
+++ Unit1 - JavaScript/Part3/11.fetch.js
@@ -1,13 +1,15 @@
 "use strict";
 
 const tbody = document.querySelector("#tabla tbody");
+const form = document.getElementById("formProduct");
+const imgPreview = document.getElementById("imgPreview");
 
 function deleteProd(e) {
-    fetch(
-      `https://api.fullstackpro.es/products-example/products/${e.target.dataset.id}`,
-      { method: "DELETE" }
-    ).then(resp => e.target.closest("tr").remove());
-  }
+  fetch(
+    `https://api.fullstackpro.es/products-example/products/${e.target.dataset.id}`,
+    { method: "DELETE" }
+  ).then((resp) => e.target.closest("tr").remove());
+}
 
 function showProduct(product) {
   const tr = document.createElement("tr");
@@ -55,3 +57,36 @@ function showProduct(product) {
 fetch("https://api.fullstackpro.es/products-example/products")
   .then((resp) => resp.json())
   .then((json) => json.products.forEach((p) => showProduct(p)));
+
+form.fileName.addEventListener("change", e => {
+  const file = form.fileName.files[0];
+  if(file) {
+    const fileReader = new FileReader();
+    fileReader.readAsDataURL(file);
+
+    fileReader.addEventListener("load", e => {
+      imgPreview.src = fileReader.result;
+    });
+  }
+});
+
+form.addEventListener("submit", e => {
+  e.preventDefault();
+
+  const product = {
+    description: form.description.value,
+    price: +form.price.value,
+    imageUrl: imgPreview.src
+  };
+
+  fetch('https://api.fullstackpro.es/products-example/products', {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(product)
+  }).then(resp => resp.json())
+  .then(json => {
+    showProduct(json.product);
+    form.reset();
+    imgPreview.src = "";
+  });
+});
