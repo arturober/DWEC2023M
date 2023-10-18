2daba032ca30f91cef1c8be78652d8da2305e451
Arturo Bernal <arturober@gmail.com>
2023-10-18 09:48:38 +0200

    vite

---
+++ Unit1 - JavaScript/Part4/example NPM/index.js
@@ -0,0 +1,6 @@
+import "./node_modules/dayjs/dayjs.min.js";
+
+
+console.log("Hello world!");
+// eslint-disable-next-line no-undef
+console.log(dayjs("2023-10-12").format("DD/MM/YYYY")); // '25/08/2023'
---
+++ Unit1 - JavaScript/Part4/vite-products/src/product-service.js
@@ -0,0 +1,21 @@
+import { SERVER } from "./constants.js";
+import { Http } from "./http.class.js";
+
+export class ProductService {
+    #http = new Http();
+
+    async getAll() {
+        const resp = await this.#http.get(`${SERVER}/products`);
+        return resp.products;
+    }
+
+    async insert(product) {
+        const resp = await this.#http.post(`${SERVER}/products`, product);
+        return resp.product;
+    }
+
+    async delete(id) {
+        await this.#http.delete(`${SERVER}/products/${id}`);
+    }
+}
+
