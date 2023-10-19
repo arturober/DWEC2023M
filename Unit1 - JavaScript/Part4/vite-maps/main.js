5ca682bc4f11108a558988231b85397e15f4b5a9
Arturo Bernal <arturober@gmail.com>
2023-10-19 09:26:55 +0200

    maps

--- Unit1 - JavaScript/Part4/vite-maps/main.js
+++ Unit1 - JavaScript/Part4/vite-maps/main.js
@@ -1,6 +1,13 @@
 import { MyGeolocation } from "./myGeolocation";
 
+const API_KEY = "An8JNymYeoGzMUqXfVJlMm_9CLeMcpx_5NB0N1G9cUEUxIadv7XX5zVc008au1N1";
+
 async function showStaticMap() {
   const coords = await MyGeolocation.getGeolocation();
-  
+  const latlon = coords.latitude + "," + coords.longitude;
+
+  const imgUrl = `https://dev.virtualearth.net/REST/v1/Imagery/Map/Road/${latlon}/15?mapSize=600,300&pp=${latlon};66&mapLayer=Basemap,Buildings&key=${API_KEY}`;
+  document.getElementById("imgMap").src = imgUrl;
 }
+
+showStaticMap();
