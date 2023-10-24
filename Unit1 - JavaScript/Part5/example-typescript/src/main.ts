46678dee4bd491ef77b449fcf00c8ee84342b049
Arturo Bernal <arturober@gmail.com>
2023-10-24 08:46:19 +0200

    typescript

--- Unit1 - JavaScript/Part5/example-typescript/src/main.ts
+++ Unit1 - JavaScript/Part5/example-typescript/src/main.ts
@@ -1,7 +1,16 @@
-let v: string = "hello";
+const v: string = "hello";
+console.log(v);
 
-function sum(n1: number, n2: number): number {
-  return n1 + n2;
+function sum(
+    n1: number,
+    n2: number,
+    n3: number,
+    n4: number,
+    n5: number
+): number {
+    return n1 + n2 + n3 + n4 + n5;
 }
+console.log(sum(3, 5));
 
-let a: string[] = ["Hello", "Bye"];
+const a: string[] = ["Hello", "Bye"];
+console.log(a);
