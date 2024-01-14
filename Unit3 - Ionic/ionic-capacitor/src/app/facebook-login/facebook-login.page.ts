f3e30cb5d4864e5335701558b8581299ad91f678
Arturo Bernal <arturo@email.com>
2024-01-14 23:15:53 +0100

    capacitor

--- Unit3 - Ionic/ionic-capacitor/src/app/facebook-login/facebook-login.page.ts
+++ Unit3 - Ionic/ionic-capacitor/src/app/facebook-login/facebook-login.page.ts
@@ -1,20 +1,75 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
+import {
+  IonButton,
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonInput,
+  IonItem,
+  IonLabel,
+  IonMenuButton,
+  IonTitle,
+  IonToolbar,
+  IonList,
+  IonIcon,
+  IonAvatar,
+  IonImg,
+  IonRow,
+  IonCol,
+} from '@ionic/angular/standalone';
+import {
+  FacebookLogin,
+  FacebookLoginResponse,
+} from '@capacitor-community/facebook-login';
 
 @Component({
   selector: 'app-facebook-login',
   templateUrl: './facebook-login.page.html',
   styleUrls: ['./facebook-login.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [
+    IonButton,
+    IonButtons,
+    IonContent,
+    IonHeader,
+    IonInput,
+    IonItem,
+    IonLabel,
+    IonMenuButton,
+    IonTitle,
+    IonToolbar,
+    IonList,
+    IonIcon,
+    IonAvatar,
+    IonImg,
+    IonRow,
+    IonCol,
+  ],
 })
 export class FacebookLoginPage implements OnInit {
+  accessToken = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  async ngOnInit() {
+    const resp =
+      (await FacebookLogin.getCurrentAccessToken()) as FacebookLoginResponse;
+    if (resp.accessToken) {
+      this.accessToken = resp.accessToken.token;
+    }
   }
 
+  async login() {
+    const resp = (await FacebookLogin.login({
+      permissions: ['email'],
+    })) as FacebookLoginResponse;
+    if (resp.accessToken) {
+      this.accessToken = resp.accessToken.token;
+    }
+  }
+
+  async logout() {
+    await FacebookLogin.logout();
+    this.accessToken = '';
+  }
 }
